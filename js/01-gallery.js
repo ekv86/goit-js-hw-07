e7368cabbb6e404702a81c17c59c6c93e0b4008f
ekv86 <ekv8686@gmail.com>
2023-05-29 23:00:19 +0300

    підключені сторонні бібліотеки

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, description, original }) =>
+    `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source = "${original}"
+      alt="${description}"/></a>
+    </li>`).join("");
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+galleryEl.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains("gallery__image")) {
+        return
+    }
+
+    const imgId = evt.target.dataset.source;
+    const currentItem = galleryItems.find(({ original }) => original === imgId);
+    
+    const instance = basicLightbox.create(`
+    <img src="${currentItem.original}" width="800" height="600">`)
+
+    instance.show();
+
+    window.addEventListener('keydown', onEscPress);
+
+    function onEscPress(evt) {
+        const visibleModal = instance.visible();
+
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+        if (!visibleModal) {
+            window.removeEventListener('keydown', onEscPress);
+        }
+        
+        console.log(evt.code);
+
+    }
+
+
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, description, original }) =>
+    `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    </li>`).join("");
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+
+    });
